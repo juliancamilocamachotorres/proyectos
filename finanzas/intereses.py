1e2171a1a93e969d118b3a628883433f737aa9c8
“chutra” <juliancamilo3099@hotmail.es>
2025-03-12 08:55:15 -0500

    presentacion modo moneda

--- finanzas/intereses.py
+++ finanzas/intereses.py
@@ -1,10 +1,13 @@
+def numeroDinero(numero)->float:
+	return f"${numero:,.2f}"
+
 def rendimientos(interesea:float,valorinicial:float,dias:int)->int:
 	interesdiario=(1+interesea)**(1/365)-1
 	total=valorinicial
 	dia=0
 	while dia < dias:
 		total+=total*interesdiario
-		print(total)
+		print("Dia: "+str(dia)+" total: "+ numeroDinero(total))
 		dia+=1
 		
 	return total
