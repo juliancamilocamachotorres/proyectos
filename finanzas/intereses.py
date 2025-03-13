e55e6f81fcc058c9d2d5eb4fed70f1af7659be73
“chutra” <juliancamilo3099@hotmail.es>
2025-03-12 19:44:01 -0500

    arreglando la interfaz de terminal

--- finanzas/intereses.py
+++ finanzas/intereses.py
@@ -4,12 +4,17 @@ def numeroDinero(numero)->float:
 def rendimientos(interesea:float,valorinicial:float,dias:int)->int:
 	interesdiario=(1+interesea)**(1/365)-1
 	total=valorinicial
-	dia=0
-	while dia < dias:
+	dia=1
+	while dia <= dias:
 		total+=total*interesdiario
 		print("Dia: "+str(dia)+" total: "+ numeroDinero(total))
 		dia+=1
 		
 	return total
 	
-print(rendimientos(0.095,8000000,150))
+
+if __name__ == "__main__":
+	porcentaje = float(input("Dime el interes efectivo anual: "))
+	dinero = float(input("Dime la cantidad de dinero: "))
+	dias = int(input("Dime dime el tiempo en dias que va estar: "))
+	rendimientos(porcentaje,dinero,dias)
